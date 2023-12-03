d8dae60fc7534d68aab887a8214384145bc0af24
Martin-Aps <149533017+Martin-Aps@users.noreply.github.com>
2023-12-03 08:26:38 +0330

    Add files via upload

---
+++ Library/Library.py
@@ -0,0 +1,16 @@
+from Database import Library
+
+mydb=Library()
+mydb.add_books(add_book=('Golestan', 'Saadi', 1990, 250, 1))
+mydb.add_books(add_book=('Bostan', 'Saadi', 1998, 367, 3))
+mydb.add_books(add_book=('Shah Name', 'Ferdousi', 1921, 754, 2))
+#mydb.books_list()
+#mydb.delete_book(1)
+#mydb.search_author('Saadi')
+#mydb.search_book('Golestan')
+#mydb.add_member(add_member=('Vahid', 23, 100))
+#mydb.search_member('Vahid')
+#mydb.delete_member(1)
+#mydb.create_borrow(member_id=(100), book_id=(1))
+#mydb.borrow_list()
+
